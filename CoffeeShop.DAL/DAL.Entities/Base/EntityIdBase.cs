b3b2beae6a8ed94684425ad0903ec18e5ed13212
Eskender Bilyalov <megusta.96@mail.ru>
2019-07-02 07:54:26 +0300

    add BLL.Entities

---
+++ CoffeeShop.DAL/DAL.Entities/Base/EntityIdBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities.Base
+{
+    public abstract class EntityIdBase
+    {
+        /// <summary>
+        /// Индентификатор
+        /// </summary>
+        public long Id { get; set; }
+    }
+}
