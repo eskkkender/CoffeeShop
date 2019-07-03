d85b9898164c2ed2bed3bd5eb53b2a624ba60cdc
Eskender Bilyalov <e.bilyalov@solarl.ru>
2019-07-03 13:38:19 +0300

    Добавлены DAL.Entities, DAL.Abstraction, DAL.EntityFramework, DAL.Stub

---
+++ DAL/DAL.Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Product
+    {
+        /// <summary>
+        /// Наименовние кофе
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Описание кофе
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
