6ef2cae71638bba852f0cd864fe3c8f1d4185b2e
Eskender Bilyalov <megusta.96@mail.ru>
2019-07-27 15:39:02 +0300

    Удалены первые проекты

---
+++ BLL/BLL.DTO/BaseDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BLL.DTO
+{
+    public class BaseDTO
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
