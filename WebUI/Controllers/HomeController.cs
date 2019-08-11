a9585082a987270aeea2baaa7f935ee05ea18020
Eskender Bilyalov <megusta.96@mail.ru>
2019-08-12 00:42:49 +0300

    Add WebUI

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL.Interfaces;
+using BLL.Services;
+using Castle.MicroKernel.Lifestyle;
+using DAL.EntityFramework;
+
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            //MapperConfigurator.Configure();
+            var container = Windsor.Container;           
+            FactoryRepository ds = new FactoryRepository(container);
+            container.Install();
+            Windsor.Initialize();
+            using (container.BeginScope())
+            {
+                var service = ds.Repository<IProductManager>();
+                // Получение всех товаров
+                var allAdverts = service.GetAllProducts();
+                ViewBag.Message = "Это вызов частичного представления из обычного";
+                ViewBag.Hell = allAdverts;
+                return View(allAdverts);
+            }
+               
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebUI/Global.asax.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using BLL.Services;
+using System.Data.Entity;
+
+namespace WebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            MapperConfigurator.Configure();
+            //var container = Windsor.Container;
+            //container.Install();
+            //Windsor.Initialize();        
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
