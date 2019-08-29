e2bd91a4d56d3cc79080ec5e7c237357a819d4f9
Eskender Bilyalov <megusta.96@mail.ru>
2019-08-29 08:39:54 +0300

    +

--- WebUI/Controllers/TestController.cs
+++ WebUI/Controllers/TestController.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using BLL.Interfaces;
+using DTO;
+
 
 namespace WebUI.Controllers
 {
     public class TestController : ApiController
     {
-        public TestController() { }
+
+        protected IProductManager Service;
+
+        public TestController(IProductManager service)
+        {
+            Service = service;
+        }
 
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var a = new ProductDTO();
+
+            
+            var a = Service.GetAllProducts();
+            // return new string[] { "value1", "value2" };
+            return Ok(a);
         }
 
         // GET api/<controller>/5
