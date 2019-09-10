4688ff49f9512de514fc76353fd5ec6220c7b179
Eskender Bilyalov <e.bilyalov@solarl.ru>
2019-09-10 18:42:18 +0300

    Добалена загрузка фото

--- WebUI/Controllers/ProductController.cs
+++ WebUI/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -32,7 +35,7 @@ namespace WebUI.Controllers
         }
 
         // POST: api/Test
-        public IHttpActionResult PostProduct(ProductDTO product)
+        public HttpResponseMessage PostProduct()
         {
             //if (!ModelState.IsValid)
             //{
@@ -41,7 +44,7 @@ namespace WebUI.Controllers
 
             //if (ModelState.IsValid)
             //{
-   
+
             //        if (FileUrl != null)
             //        {
             //            string FileName = Path.GetFileName(FileUrl.FileName);
@@ -55,9 +58,51 @@ namespace WebUI.Controllers
 
             //}
 
-            Service.AddProduct(product);
+            //Service.AddProduct(product);
 
-            return CreatedAtRoute("DefaultApi", new { id = product.Id }, product);
+            //if (HttpContext.Current.Request.Files.AllKeys.Any())
+            //{
+            // // Get the uploaded image from the Files collection
+            // var httpPostedFile = HttpContext.Current.Request.Files["UploadedImage"];
+
+            // if (httpPostedFile != null)
+            // {
+            // // Get the complete file path
+            // String fileSavePath = HttpContext.Current.Server.MapPath("~/Content/images/") + HttpContext.Current.Request.Form["ImageName"];
+
+            // // Save the uploaded file to "UploadedFiles" folder
+            // httpPostedFile.SaveAs(fileSavePath);
+            // }
+            //}
+
+            HttpResponseMessage result;
+            var httpRequest = HttpContext.Current.Request;
+
+            // Check if files are available
+            //if (httpRequest.Files.Count > 0)
+            //{
+            var files = new List<string>();
+
+            // interate the files and save on the server
+
+            foreach (string file in httpRequest.Files)
+            {
+                var postedFile = httpRequest.Files[file];
+                var filePath = HttpContext.Current.Server.MapPath("~/Content/images/" + postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                files.Add(filePath);
+            }
+
+            // return result
+            result = Request.CreateResponse(HttpStatusCode.Created, files);
+            //}
+            //else
+            //{
+            // // return BadRequest (no file(s) available)
+            // result = Request.CreateResponse(HttpStatusCode.BadRequest);
+            //}
+            return result;
         }
 
         public IHttpActionResult DeleteProduct(int id)
